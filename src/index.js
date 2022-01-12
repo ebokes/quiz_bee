c72febf8b43305dda3577cc6cc822eb20f2a4896
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-01-12 09:38:06 +0100

    Checks to see if the option selected matches the answers

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import QuestionBox from "./components/QuestionBox";
 class QuizBee extends Component {
   state = {
     questionBank: [],
+    score: 0,
+    responses: 0,
   };
 
   getQuestions = () => {
@@ -17,6 +19,16 @@ class QuizBee extends Component {
     });
   };
 
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      this.setState({
+        score: this.state.score + 1,
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5,
+    });
+  };
   componentDidMount() {
     this.getQuestions();
   }
@@ -26,15 +38,18 @@ class QuizBee extends Component {
       <div className="container">
         <div className="title">QuizBee</div>
         {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
           this.state.questionBank.map(
             ({ question, answers, correct, questionId }) => (
               <QuestionBox
                 question={question}
                 options={answers}
                 key={questionId}
+                selected={(answer) => this.computeAnswer(answer, correct)}
               />
             )
           )}
+        {this.state.responses === 5 ? <h2>{this.state.score}</h2> : null}
       </div>
     );
   }
