bbb9ae798d8c8c798f5d2fe8ffeb878c43942ba2
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-01-12 09:02:30 +0100

    Added options to the questions

---
+++ src/components/QuestionBox2.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class QuestionBox2 extends Component {
+  state = {
+    answer: options,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default QuestionBox2;
